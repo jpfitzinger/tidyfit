bd16a59754017cca082b54ef9984075306f5d1fb
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-05-13 17:43:48 +0200

    Updated tidyfit

--- R/fit.R
+++ R/fit.R
@@ -1,9 +1,11 @@
 
 
-.fit <- function(.data, formula, model_list, .cv, .cv_args, .hyper_grid, .weights) {
+.fit <- function(.data, formula, model_list, .cv, .cv_args, .hyper_grid, .weights, gr_vars, .mask) {
 
-  ff = as.formula("Growth ~ .")
-  m <- model.frame(formula = ff, data = .data)
+  .data <- .data %>%
+    select(-!!gr_vars, -!!.mask)
+
+  m <- model.frame(formula = formula, data = .data)
 
   # Prepare CV
   if (.cv == "none")
@@ -17,37 +19,48 @@
   result <-
     map2_dfr(model_list, names(model_list), function(model, nam) {
 
-      result <- cv$splits %>%
-        furrr::future_map_dfr(function(split) {
+      if (.cv != "none") {
 
-          if ("rsplit" %in% class(split)) {
+        result <- cv$splits %>%
+          furrr::future_map_dfr(function(split) {
+
             df_train <- rsample::training(split)
             df_test <- rsample::testing(split)
-          } else {
-            df_train <- split
-            df_test <- NULL
-          }
 
-          beta <- model(df_train[, -1], df_train[, 1], .hyper_grid)
-          if (!is.null(df_test)) {
-            df_test <- cbind(1, data.matrix(df_test))
-            mse <- mean((df_test[, 1] - df_test[, -1] %*% beta)^2)
-          } else {
-            mse <- 0
-          }
+            beta <- do.call(model,
+                            append(list(x = df_train[,-1], y = df_train[,1]),
+                                   .hyper_grid))
+            beta <- beta %>%
+              select(grid_id, beta, variable) %>%
+              spread(grid_id, beta)
 
-          out <- tibble(
-            variable = names(beta),
-            beta = beta,
-            mse = mse
-          )
+            x_test <- data.frame(`(Intercept)` = 1, data.matrix(df_test[, -1]), check.names = FALSE)
+            x_test <- x_test[, beta$variable]
+            beta <- beta[, -1]
+            fit <- data.matrix(x_test) %*% data.matrix(beta)
+            mse <- colMeans((df_test[, 1] - fit)^2)
 
-        })
+          })
+
+        best_grid <- colnames(result)[which.min(colMeans(result))]
+
+      } else {
+
+        best_grid <- NULL
+
+      }
+
+      result <- do.call(model,
+                      append(list(x = m[,-1], y = m[,1]),
+                             .hyper_grid))
+      if (!is.null(best_grid)) {
+        result <- result %>%
+          filter(as.character(grid_id) == as.character(best_grid))
+      }
 
       result <- result %>%
-        filter(mse == min(mse, na.rm = TRUE)) %>%
-        select(-mse) %>%
-        mutate(model = nam)
+        mutate(model = nam) %>%
+        select(-grid_id)
 
       return(result)
 
---
+++ R/tidyfit.R
@@ -0,0 +1,61 @@
+#' @name tidyfit
+#' @title Linear model estimation on tidy data
+#' @description This function is a wrapper to fit many different types of linear
+#' regression of classification model on a (potentially grouped) `tibble`.
+#'
+#' @details
+#'
+#' @param x Input matrix or data.frame, of dimension \eqn{(N\times p)}{(N x p)}; each row is an observation vector.
+#' @param ...  Additional arguments passed to \code{hclust}.
+#' @return A \code{tibble}.
+#' @author Johann Pfitzinger
+#' @references
+#'
+#' @examples
+#'
+#' @export
+#'
+#' @seealso \code{tidypredict} method
+#'
+#' @importFrom magrittr `%>%`
+
+tidyfit <- function(
+  .data,
+  formula,
+  ...,
+  .cv = c("none", "loo", "vfold"),
+  .cv_args = list(v = 10),
+  .hyper_grid = NULL,
+  .weights = NULL,
+  .mask = NULL
+) {
+
+  # TODO: check names
+
+  model_list <- list(...)
+  .cv <- match.arg(.cv)
+  if (is.null(.hyper_grid)) .hyper_grid <- list()
+
+  gr_vars <- group_vars(.data)
+
+  df <- .data %>%
+    do(result = .fit(., formula, model_list, .cv, .cv_args,
+                     .hyper_grid, .weights, gr_vars, .mask)) %>%
+    unnest(result)
+
+  df <- df %>%
+    group_by(model) %>%
+    do(temp = select(., where(~!all(is.na(.)))))
+
+  df <- df$temp %>%
+    map_dfr(~nest(., model_info = -c(!!gr_vars, variable, beta, model)))
+
+  df <- df %>%
+    group_by(across(all_of(gr_vars)))
+
+  attr(df, "formula") <- formula
+  attr(df, "structure") <- list(mask = .mask, col_names = colnames(df))
+
+  return(df)
+
+}
