a02d3d5d87bc99e3c5e2b131ff666865a1219799
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-05-14 13:24:27 +0200

    Return internal cv results from slices

--- R/fit.R
+++ R/fit.R
@@ -1,6 +1,6 @@
 
 
-.fit <- function(.data, formula, model_list, .cv, .cv_args, .hyper_grid, .weights, gr_vars, .mask) {
+.fit <- function(.data, formula, model_list, .cv, .cv_args, .hyper_grid, .weights, gr_vars, .mask, .return_slices) {
 
   .data <- .data %>%
     select(-!!gr_vars, -!!.mask)
@@ -11,9 +11,11 @@
   if (.cv == "none")
     cv <- tibble(splits = list(m))
   if (.cv == "vfold")
-    cv <- rsample::vfold_cv(m, v = .cv_args$v)
+    cv <- do.call(rsample::vfold_cv, append(list(data = m), .cv_args))
   if (.cv == "loo")
-    cv <- rsample::loo_cv(m)
+    cv <- cv <- do.call(rsample::loo_cv, append(list(data = m), .cv_args))
+  if (.cv == "ts")
+    cv <- cv <- do.call(rsample::rolling_origin, append(list(data = m), .cv_args))
 
   # Evaluate methods
   result <-
@@ -22,15 +24,15 @@
       if (.cv != "none") {
 
         result <- cv$splits %>%
-          furrr::future_map_dfr(function(split) {
+          furrr::future_map(function(split) {
 
             df_train <- rsample::training(split)
             df_test <- rsample::testing(split)
 
-            beta <- do.call(model,
+            result <- do.call(model,
                             append(list(x = df_train[,-1], y = df_train[,1]),
                                    .hyper_grid))
-            beta <- beta %>%
+            beta <- result %>%
               select(grid_id, beta, variable) %>%
               spread(grid_id, beta)
 
@@ -40,9 +42,13 @@
             fit <- data.matrix(x_test) %*% data.matrix(beta)
             mse <- colMeans((df_test[, 1] - fit)^2)
 
+            return(list(mse = mse, result = result))
+
           })
 
-        best_grid <- colnames(result)[which.min(colMeans(result))]
+        mse <- result %>% map_dfr(function(res) res$mse)
+        result <- result %>% map_dfr(function(res) res$result)
+        best_grid <- colnames(mse)[which.min(colMeans(mse))]
 
       } else {
 
@@ -50,9 +56,12 @@
 
       }
 
-      result <- do.call(model,
-                      append(list(x = m[,-1], y = m[,1]),
-                             .hyper_grid))
+      if (!.return_slices) {
+        result <- do.call(model,
+                          append(list(x = m[,-1], y = m[,1]),
+                                 .hyper_grid))
+      }
+
       if (!is.null(best_grid)) {
         result <- result %>%
           filter(as.character(grid_id) == as.character(best_grid))
