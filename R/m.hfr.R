b217abd1fb77f1ff3899f0f884ef783d6f334631
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-05-18 19:48:26 +0200

    hfr method

---
+++ R/m.hfr.R
@@ -0,0 +1,51 @@
+#' @name m.lasso
+#' @title Linear regression for tidyfit
+#' @description Fits a linear regression and returns the results as a tibble. The function can be used with \code{tidyfit}.
+#'
+#' @details
+#'
+#' @param x Input matrix or data.frame, of dimension \eqn{(N\times p)}{(N x p)}; each row is an observation vector.
+#' @param y Response variable.
+#' @param ...  Additional arguments passed to \code{lm}.
+#' @return A 'tibble'.
+#' @author Johann Pfitzinger
+#' @references
+#'
+#' @examples
+#' x = matrix(rnorm(100 * 20), 100, 20)
+#' y = rnorm(100)
+#' fit = hfr(x, y, kappa = 0.5)
+#' coef(fit)
+#'
+#' @export
+#'
+#' @seealso \code{tidypredict} method
+#'
+#' @importFrom hfr cv.hfr
+
+# x = matrix(rnorm(100 * 20), 100, 20)
+# y = rnorm(100)
+# m.lm(x, y, .ctr = list())
+
+m.hfr <- function(x, y, kappa = NULL, ...) {
+
+  args <- list(...)
+  args <- args[names(args) %in% names(formals(hfr::cv.hfr))]
+
+  if (is.null(kappa)) kappa <- seq(0, 1, by = 0.1)
+
+  m <- do.call(hfr::cv.hfr, append(list(x = x, y = y, nfolds = 1,
+                                        kappa_grid = kappa), args))
+
+  coefs <- coef(m)
+
+  out <- coefs %>%
+    as_tibble %>%
+    mutate(variable = rownames(coefs)) %>%
+    mutate(variable = ifelse(variable == "intercept", "(Intercept)", variable)) %>%
+    gather("kappa", "beta", -variable) %>%
+    mutate(grid_id = kappa)
+
+  return(out)
+
+}
