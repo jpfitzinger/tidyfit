ff83dadc11d34063ab2e0528422e60121b212e1f
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-07-17 17:24:37 +0200

    Update PLSR to allow partialising, updated docs

--- R/mod_plsr.R
+++ R/mod_plsr.R
@@ -1,40 +1,64 @@
-#' @name m.plsr
-#' @title Partial Least Squares Regression for tidyfit
-#' @description Fits a partial least squares regression and returns the results as a tibble. The function can be used with \code{tidyfit}.
+#' @name mod_plsr
+#' @title Partial Least Squares Regression for \code{tidyfit}
+#' @description Fits a partial least squares regression and returns the results as a tibble. The function can be used with \code{regress}.
 #'
 #' @details The partial least squares regression is fitted using \code{pls} package. Covariates are standardized, with coefficients back-transformed to the original scale.
+#'
+#' If no hyperparameter grid is passed (\code{ncomps = NULL}), the default is \code{seq(1, nvars)}, where nvars is the number of features.
+#'
+#' When called without \code{x} and \code{y} arguments, the function returns a partialised version of the function that can be called with data to fit the model.
 #'
 #' @param x Input matrix or data.frame, of dimension \eqn{(N\times p)}{(N x p)}; each row is an observation vector.
 #' @param y Response variable.
 #' @param ncomp number of components or sequence of components.
-#' @param ...  Additional arguments passed to \code{lm}.
+#' @param ...  Additional arguments passed to \code{pls::plsr}.
 #' @return A 'tibble'.
 #' @author Johann Pfitzinger
 #' @references
 #'
 #' @examples
-#' x = matrix(rnorm(100 * 20), 100, 20)
-#' y = rnorm(100)
-#' fit = m.plsr(x, y, ncomp = 4)
+#' x <- matrix(rnorm(100 * 20), 100, 20)
+#' y <- rnorm(100)
+#' fit <- mod_plsr(x, y, ncomp = 4)
 #' fit
 #'
 #' @export
 #'
-#' @seealso \code{m.pcr} method
+#' @seealso \code{mod_pcr} method
 #'
 #' @importFrom pls plsr
 #' @importFrom stats coef sd
 #' @importFrom dplyr as_tibble mutate
 #' @importFrom tidyr gather
 #' @importFrom rlang .data
+#' @importFrom purrr partial
+#' @importFrom dials grid_regular penalty
 
 
-m.plsr <- function(x, y, ncomp = NULL, ...) {
+mod_plsr <- function(
+    x = NULL,
+    y = NULL,
+    ncomp = NULL,
+    ...
+    ) {
+
+  # Return a partial if no data is provided
+  if (is.null(x) & is.null(y)) {
+
+    args <- c(as.list(environment()), list(...))
+    args <- args[!names(args) %in% c("x", "y")]
+    args <- append(args, list(.f = tidyfit::mod_plsr))
+    return(do.call(purrr::partial, args))
+
+  }
 
   args <- list(...)
   if (!is.null(args$family)) {
     if (args$family == "binomial")
       stop("plsr cannot be used for classification")
+  }
+  if ("weights" %in% names(args)) {
+    warning("pcr cannot handle weights, weights are ignored")
   }
   args <- args[names(args) %in% names(formals(pls::plsr))]
 
