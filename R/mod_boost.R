b7ae7629e03304677eada70ebef3a482f44b8814
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-07-21 16:32:21 +0200

    Update gradient boosting to allow partialising, updated docs

--- R/mod_boost.R
+++ R/mod_boost.R
@@ -1,10 +1,14 @@
-#' @name m.boost
-#' @title Gradient boosting regression for tidyfit
-#' @description Fits a gradient boosting regression and returns the results as a tibble. The function can be used with \code{regress}.
+#' @name mod_boost
+#' @title Gradient boosting regression for \code{tidyfit}
+#' @description Fits a gradient boosting regression or classification and returns the results as a tibble. The function can be used with \code{regress}.
 #'
 #' @details The gradient boosting regression is performed using 'mboost::glmboost'.
 #'
 #' The function can be used for classification or regression, covariates are standardized and an intercept is always included.
+#'
+#' If no hyperparameter grid is passed (\code{mstop = NULL}), the default grid is used with \code{mstop = c(100, 500, 1000, 2000)}.
+#'
+#' When called without \code{x} and \code{y} arguments, the function returns a partialised version of the function that can be called with data to fit the model.
 #'
 #' @param x input matrix or data.frame, of dimension \eqn{(N\times p)}{(N x p)}; each row is an observation vector.
 #' @param y response variable.
@@ -17,23 +21,38 @@
 #' mboost: Model-Based Boosting, R package version 2.9-7, https://CRAN.R-project.org/package=mboost.
 #'
 #' @examples
-#' x = matrix(rnorm(100 * 20), 100, 20)
-#' y = rbinom(100, 1, 0.5)
-#' fit = m.boost(x, y, mstop = 100, family = "binomial")
+#' x <- matrix(rnorm(100 * 20), 100, 20)
+#' y <- rbinom(100, 1, 0.5)
+#' fit <- mod_boost(x, y, mstop = 100, family = "binomial")
 #' fit
 #'
 #' @export
 #'
 #' @seealso \code{tidypredict} method
 #'
-#' @importFrom stats coef
 #' @importFrom mboost glmboost Gaussian Binomial
-#' @importFrom purrr map_dbl
+#' @importFrom purrr map_dbl partial quietly
 #' @importFrom dplyr mutate as_tibble
 #' @importFrom tidyr gather
 #' @importFrom rlang .data
+#' @importFrom dials grid_regular penalty mixture
 
-m.boost <- function(x, y, mstop = NULL, ...) {
+mod_boost <- function(
+    x = NULL,
+    y = NULL,
+    mstop = NULL,
+    ...
+    ) {
+
+  # Return a partial if no data is provided
+  if (is.null(x) & is.null(y)) {
+
+    args <- c(as.list(environment()), list(...))
+    args <- args[!names(args) %in% c("x", "y")]
+    args <- append(args, list(.f = tidyfit::mod_boost))
+    return(do.call(purrr::partial, args))
+
+  }
 
   args <- list(...)
   if (!is.null(args$family)) {
@@ -51,22 +70,34 @@ m.boost <- function(x, y, mstop = NULL, ...) {
 
   if (is.null(mstop)) mstop <- c(100, 500, 1000, 2000)
 
-  x <- data.frame(intercept = 1, x, check.names = FALSE)
+  standard_mean <- apply(x, 2, mean)
+  standard_sd <- apply(x, 2, stats::sd)
+  xs <- as.matrix(scale(x, center = standard_mean, scale = standard_sd))
+
+  xs <- data.frame(intercept = 1, xs, check.names = FALSE)
+
+  quiet_coefs <- purrr::quietly(mboost:::coef.glmboost)
 
   coefs <- sapply(mstop, function(mst) {
     ctr <- mboost::boost_control(mstop = mst, nu = 0.1)
-    m <- do.call(mboost::glmboost, append(list(x = data.matrix(x), y = y,
-                                               control = ctr, center = T,
+    m <- do.call(mboost::glmboost, append(list(x = data.matrix(xs), y = y,
+                                               control = ctr, center = F,
                                                family = family), args))
-    beta <- purrr::map_dbl(colnames(x), function(x) stats::coef(m, which = x, off2int = F)[x])
-    beta[1] <- beta[1] + m$offset
-    names(beta) <- colnames(x)
 
+    beta <- purrr::map_dbl(colnames(xs), function(x) quiet_coefs(m, which = x, off2int = F)$result[x])
+    beta[1] <- beta[1] + m$offset
+    names(beta) <- colnames(xs)
+    beta <- beta * 2
+    beta[-1] <- beta[-1] / standard_sd
+    beta[1] <- beta[1] - crossprod(beta[-1], standard_mean)
     return(beta)
   })
   colnames(coefs) <- as.character(mstop)
 
   var_names <- rownames(coefs)
+
+  grid_ids <- paste0("s", seq_along(mstop))
+  names(grid_ids) <- mstop
 
   out <- coefs %>%
     data.matrix %>%
@@ -74,7 +105,8 @@ m.boost <- function(x, y, mstop = NULL, ...) {
     dplyr::mutate(variable = var_names) %>%
     tidyr::gather("grid_id", "beta", -.data$variable) %>%
     dplyr::mutate(variable = ifelse(.data$variable == "intercept", "(Intercept)", .data$variable)) %>%
-    dplyr::mutate(mstop = mstop[match(.data$grid_id, colnames(coefs))])
+    dplyr::mutate(mstop = mstop[match(.data$grid_id, colnames(coefs))]) %>%
+    mutate(grid_id = grid_ids[.data$grid_id])
 
   return(out)
 
