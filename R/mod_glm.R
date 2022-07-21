529edbfc3397be4198dbea9260b93422a38aefa7
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-07-21 12:14:45 +0200

    Update glm to allow partialisation, updated docs

--- R/mod_glm.R
+++ R/mod_glm.R
@@ -1,30 +1,42 @@
-#' @name m.glm
-#' @title Logistic regression for tidyfit
-#' @description Fits a logistic regression and returns the results as a tibble. The function can be used with \code{tidyfit}.
+#' @name mod_glm
+#' @title Logistic regression for \code{tidyfit}
+#' @description Fits a logistic regression and returns the results as a tibble. The function can be used with \code{regress}.
 #'
 #' @details
 #'
 #' @param x Input matrix or data.frame, of dimension \eqn{(N\times p)}{(N x p)}; each row is an observation vector.
 #' @param y Response variable.
-#' @param ...  Additional arguments passed to \code{lm}.
+#' @param ...  Additional arguments passed to \code{glm}.
 #' @return A 'tibble'.
 #' @author Johann Pfitzinger
 #' @references
 #'
 #' @examples
-#' x = matrix(rnorm(100 * 20), 100, 20)
-#' y = rbinom(100, 1, 0.5)
-#' fit = m.glm(x, y)
+#' x <- matrix(rnorm(100 * 20), 100, 20)
+#' y <- rbinom(100, 1, 0.5)
+#' fit <- mod_glm(x, y)
 #' fit
 #'
 #' @export
 #'
-#' @seealso \code{m.lm} method
+#' @seealso \code{mod_lm} method
 #'
 #' @importFrom stats glm coef binomial gaussian poisson
 #' @importFrom dplyr tibble
+#' @importFrom purrr partial
+#' @importFrom dials grid_regular penalty
 
-m.glm <- function(x, y, ...) {
+mod_glm <- function(x = NULL, y = NULL, ...) {
+
+  # Return a partial if no data is provided
+  if (is.null(x) & is.null(y)) {
+
+    args <- c(as.list(environment()), list(...))
+    args <- args[!names(args) %in% c("x", "y")]
+    args <- append(args, list(.f = tidyfit::mod_glm))
+    return(do.call(purrr::partial, args))
+
+  }
 
   args <- list(...)
   args <- args[names(args) %in% names(formals(stats::glm))]
