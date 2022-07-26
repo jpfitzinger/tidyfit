432a0411fb9c0131aebbef11807d7951311be722
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-07-26 08:44:37 +0200

    Roxygen documentation for data set

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Industry-Factor Returns Data Set
+#'
+#' @name Factor_Industry_Returns
+#' @description The data set includes monthly returns between 1963 and 2022 for 10 industries, as well as factor values for 5 Fama-French factors.
+#' @docType data
+#' @references \url{https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}
+#' @keywords data
+NULL
