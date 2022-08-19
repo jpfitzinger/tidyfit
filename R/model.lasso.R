9dafedeccf748fa886239f8e4dfa8d2b08cc5b34
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-08-19 20:13:14 +0200

    Updated lasso, ridge and enet to handle multinomial regression

--- R/model.lasso.R
+++ R/model.lasso.R
@@ -6,7 +6,9 @@
 #'
 #' - \code{lambda} *(L1 penalty)*
 #'
-#' The Lasso regression is estimated using \code{glmnet::glmnet} with \code{alpha = 1}. For classification pass \code{family = "binomial"} to \code{control} or to the argument \code{...} in \code{\link{m}}.
+#' The Lasso regression is estimated using \code{glmnet::glmnet} with \code{alpha = 1}. For classification pass \code{family = "binomial"} to \code{...} in \code{\link{m}} or use \code{\link{classify}}.
+#'
+#' If the response variable contains more than 2 classes, a multinomial response is used automatically.
 #'
 #' An intercept is always included and features are standardized with coefficients transformed to the original scale.
 #'
@@ -48,23 +50,55 @@
   control <- control[names(control) %in% methods::formalArgs(glmnet::glmnet)]
   control <- control[names(control) != "alpha"]
 
-  f <- control$family
-  control$family <- f$family
+  # Prepare 'family' arg
+  if (inherits(control$family, "character")) {
+    f_name <- control$family
+  } else {
+    f_name <- control$family$family
+  }
+  if (!f_name %in% c("gaussian", "binomial", "multinomial"))
+    stop("invalid 'family' argument")
+  if (f_name == "gaussian") {
+    control$family <- "gaussian"
+    f <- gaussian()
+  } else {
+    control$family <- "multinomial"
+    f <- binomial()
+    y <- as.factor(y)
+  }
+
+  # Sparse coefficient matrix to tibble
+  gather_coef_mat <- function(coef_mat) {
+    colnames(coef_mat) <- formatC(1:ncol(coef_mat), 2, flag = "0")
+    var_names <- rownames(coef_mat)
+    var_names[1] <- "(Intercept)"
+    out <- coef_mat %>%
+      data.matrix %>%
+      dplyr::as_tibble() %>%
+      dplyr::mutate(variable = var_names) %>%
+      tidyr::gather("grid_id", "beta", -.data$variable) %>%
+      dplyr::mutate(lambda = m$lambda[match(.data$grid_id, colnames(coef_mat))]) %>%
+      dplyr::mutate(family = list(f)) %>%
+      dplyr::select(.data$variable, .data$beta, .data$grid_id, .data$family, .data$lambda)
+    return(out)
+  }
 
   m <- do.call(glmnet::glmnet, append(list(x = x, y = y, alpha = 1), control))
 
-  coefs <- stats::coef(m)
-  var_names <- rownames(coefs)
-  colnames(coefs) <- formatC(1:ncol(coefs), 2, flag = "0")
+  coefs <- coef(m)
 
-  out <- coefs %>%
-    data.matrix %>%
-    dplyr::as_tibble() %>%
-    dplyr::mutate(variable = var_names) %>%
-    tidyr::gather("grid_id", "beta", -.data$variable) %>%
-    dplyr::mutate(lambda = control$lambda[match(.data$grid_id, colnames(coefs))]) %>%
-    mutate(family = list(f)) %>%
-    dplyr::select(.data$variable, .data$beta, .data$grid_id, .data$family, .data$lambda)
+  if (f_name == "gaussian") {
+    out <- gather_coef_mat(coefs)
+  } else {
+    lvls <- levels(y)
+    out <- purrr::map_dfr(coefs, gather_coef_mat, .id = "class")
+    if (length(lvls) == 2) {
+      out <- out %>%
+        dplyr::filter(.data$class == lvls[2]) %>%
+        dplyr::select(-.data$class) %>%
+        dplyr::mutate(beta = .data$beta * 2)
+    }
+  }
 
   control <- control[!names(control) %in% c("family", "lambda")]
   if (length(control) > 0) {
