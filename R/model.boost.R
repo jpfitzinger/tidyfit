1d23fb7fe94b0ad60a08874aaab4bf0d456533ff
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-08-21 22:06:28 +0200

    Manually add default args for mboost

--- R/model.boost.R
+++ R/model.boost.R
@@ -57,7 +57,7 @@
   nu <- control$nu
   # Available args are not accessible using formalArgs without importing mboost:::glmboost.matrix
   boost.default_args <- c("x", "y", "center", "weights", "offset", "family", "na.action", "control", "oobweights")
-  control <- control[names(control) %in% methods::formalArgs(boost.default_args)]
+  control <- control[names(control) %in% boost.default_args]
   control <- control[!names(control) %in% c("control", "center", "family")]
 
   standard_mean <- apply(x, 2, mean)
