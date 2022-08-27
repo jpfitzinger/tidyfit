27f46b6401d3e2b75c75d0ad827b345b66502119
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-08-27 16:51:58 +0200

    Internal bootstrapping function

---
+++ R/make_rsample_bootstraps.R
@@ -0,0 +1,27 @@
+
+#' @importFrom tidyr unnest nest
+#' @importFrom dplyr any_of select rename ungroup everything
+#' @importFrom tibble new_tibble
+#' @importFrom rsample int_pctl
+#' @importFrom rlang .data
+
+.make_rsample_bootstraps <- function(estimates, alpha = 0.05) {
+
+  df_boot <- estimates %>%
+    dplyr::ungroup() %>%
+    tidyr::unnest(any_of("model_info")) %>%
+    dplyr::select(any_of(c("term", "estimate", "std.error", "slice_id"))) %>%
+    dplyr::rename(id = .data$slice_id) %>%
+    tidyr::nest(result = -.data$id)
+
+  df_boot <- tibble::new_tibble(df_boot, class = c("rset", "bootstraps"))
+
+  intervals <- rsample::int_pctl(df_boot, .data$result, alpha = alpha)
+  intervals <- intervals %>%
+    dplyr::rename(estimate = .data$.estimate) %>%
+    dplyr::select(.data$term, .data$estimate, .data$.upper, .data$.lower) %>%
+    tidyr::nest(interval = dplyr::everything())
+
+  return(intervals$interval)
+
+}
