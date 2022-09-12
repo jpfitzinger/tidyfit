105c335965cb8100fc8e2dcae0dde725226bec9e
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-09-12 14:54:31 +0200

    updated TVP regression

--- R/model.tvp.R
+++ R/model.tvp.R
@@ -2,11 +2,9 @@
 #' @title Bayesian Time-Varying Regression for \code{tidyfit}
 #' @description Fits a Bayesian time-varying regression and returns the results as a tibble. The function can be used with \code{\link{regress}}.
 #'
-#' @param formula an object of class "formula": a symbolic description of the model to be fitted.
+#' @param self a tidyFit R6 class.
 #' @param data a data frame, data frame extension (e.g. a tibble), or a lazy data frame (e.g. from dbplyr or dtplyr).
-#' @param control  Additional arguments passed to \code{shrinkTVP::shrinkTVP}.
-#' @param ... Not used.
-#' @return A 'tibble'.
+#' @return A fitted tidyFit class model.
 #'
 #' @details  **Hyperparameters:**
 #'
@@ -14,7 +12,7 @@
 #'
 #' The function provides a wrapper for \code{shrinkTVP::shrinkTVP}.
 #'
-#' An argument \code{index_col} can be passed, which allows a custom index to be added to \code{coef(m("tvp"))} (e.g. a date index). This only works when the formula is specified as \code{y ~ .} (with a dot on the RHS), since otherwise the index_col is currently removed by \code{\link{regress}}.
+#' An argument \code{index_col} can be passed, which allows a custom index to be added to \code{coef(m("tvp"))} (e.g. a date index).
 #'
 #' @author Johann Pfitzinger
 #'
@@ -34,84 +32,30 @@
 #'
 #' @seealso \code{\link{.model.robust}}, \code{\link{.model.glm}} and \code{\link{m}} methods
 #'
-#' @importFrom dplyr tibble everything as_tibble
-#' @importFrom tidyr nest
-#' @importFrom purrr partial
-#' @importFrom stats quantile
+#' @importFrom purrr safely quietly
 #' @importFrom methods formalArgs
-#' @importFrom utils object.size
 
 .model.tvp <- function(
-    formula = NULL,
-    data = NULL,
-    control = NULL,
-    ...
+    self,
+    data = NULL
 ) {
-
-  idx_col <- control$index_col
-  control <- control[names(control) %in% methods::formalArgs(shrinkTVP::shrinkTVP)]
-
-  if (is.null(idx_col)) {
+  idx_col <- self$args$index_col
+  if (is.null(self$args$index_col)) {
     idx_var <- 1:nrow(data)
   } else {
-    idx_var <- data[, idx_col]
-    data <- data[, colnames(data)!=idx_col]
+    idx_var <- data[, self$args$index_col]
+    data <- data[, colnames(data)!=self$args$index_col]
   }
-
-  m <- do.call(shrinkTVP::shrinkTVP, append(list(formula = formula, data = data), control))
-
-  model_handler <- purrr::partial(.handler.shrinkTVP, object = m, formula = formula, index_var = idx_var)
-
-  control <- control[!names(control) %in% c("weights")]
-  settings <- .control_to_settings(control)
-
-  out <- tibble(
-    estimator = "shrinkTVP::shrinkTVP",
-    size = utils::object.size(m),
-    handler = list(model_handler),
-    settings
-  )
-
-  return(out)
-
-}
-
-.handler.shrinkTVP <- function(object, data, formula = NULL, index_var = NULL, ..., .what = "model") {
-
-  if (.what == "model") {
-    return(object)
+  ctr <- self$args[names(self$args) %in% methods::formalArgs(shrinkTVP::shrinkTVP)]
+  eval_fun_ <- function(...) {
+    args <- list(...)
+    do.call(shrinkTVP::shrinkTVP, args)
   }
-
-  if (.what == "predict") {
-    response_var <- all.vars(formula)[1]
-    if (response_var %in% colnames(data)) {
-      truth <- data[, response_var]
-    } else {
-      truth <- NULL
-    }
-    pred <- stats::predict(object)
-    pred <- dplyr::tibble(
-      prediction = colMeans(pred),
-      truth = truth
-    )
-    return(pred)
-  }
-
-  if (.what == "estimates") {
-    beta <- object$beta
-    estimates <- map_dfr(beta, function(bet) {
-      tibble(
-        estimate = apply(bet, 2, mean)[-1],
-        upper = apply(bet, 2, stats::quantile, 0.95)[-1],
-        lower = apply(bet, 2, stats::quantile, 0.05)[-1],
-        posterior.sd = apply(bet, 2, sd)[-1],
-        index = index_var
-      )
-    }, .id = "term")
-    estimates <- estimates %>%
-      dplyr::mutate(term = gsub("beta_", "", .data$term)) %>%
-      dplyr::mutate(term = ifelse(.data$term == "Intercept", "(Intercept)", .data$term))
-    return(estimates)
-  }
-
+  eval_fun <- purrr::safely(purrr::quietly(eval_fun_))
+  res <- do.call(eval_fun,
+                 append(list(formula = self$formula, data = data), ctr))
+  .store_on_self(self, res)
+  self$fit_info <- list(index_var = idx_var)
+  self$estimator <- "shrinkTVP::shrinkTVP"
+  invisible(self)
 }
