4001591d1a3db6066afb27ef632fbf69ee320a51
Johann Pfitzinger <jpfitzinger@xmlh.de>
2022-09-24 16:02:59 +0200

    Valid fitted values for 'glmnet' classification methods

--- R/fitted_methods.R
+++ R/fitted_methods.R
@@ -2,11 +2,36 @@
 
 #' @importFrom dplyr tibble
 
-.fitted.glmnet <- function(object, lambda = NULL, ...) {
-  fitted <- dplyr::tibble(
-    fitted = drop(predict(object, object$call$x, s = lambda))
-  )
-  return(fitted)
+.fitted.glmnet <- function(object, lambda = NULL, family = NULL, ...) {
+  x <- object$call$x
+  pred_mat <- stats::predict(object, x, type = "response", s = lambda)
+
+  if (length(dim(pred_mat))==3) {
+    class_vals <- dimnames(pred_mat)[[2]]
+  } else {
+    class_vals <- NULL
+  }
+  pred <- pred_mat %>%
+    dplyr::as_tibble() %>%
+    dplyr::mutate(row_n = dplyr::row_number())
+  if (family == "multinomial") {
+    pred <- pred %>%
+      tidyr::pivot_longer(-dplyr::any_of(c("row_n")),
+                          names_to = c("class", "grid_id"),
+                          values_to = "fitted",
+                          names_sep = "\\.")
+  } else {
+    pred <- pred %>%
+      tidyr::gather("grid_id", "fitted", -dplyr::any_of(c("row_n")))
+  }
+  pred <- pred %>%
+    dplyr::select(-.data$row_n, -.data$grid_id)
+  if (length(class_vals)==2) {
+    pred <- pred %>%
+      dplyr::filter(.data$class == sort(class_vals)[2]) %>%
+      dplyr::select(-.data$class)
+  }
+  return(pred)
 }
 
 .fitted.lm <- function(object, ...) {
