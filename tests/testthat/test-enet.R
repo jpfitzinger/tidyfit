c2afe9e2fa25f166a757f652c7b0bb3c2043c483
Johann Pfitzinger <jpfitzinger@xmlh.de>
2023-02-06 21:13:39 +0100

    Unit testing with testthat and initial tests

---
+++ tests/testthat/test-enet.R
@@ -0,0 +1,40 @@
+test_that("enet regression works", {
+  library(dplyr)
+  df_reg <- MASS::Boston
+  df_cls3 <- iris
+  df_cls2 <- iris %>% filter(Species != "setosa")
+
+  m_reg <- m("enet", medv ~ ., df_reg, lambda = c(0.1), alpha = 0.1)
+  expect_equal(nrow(coef(m_reg)), 12)
+  expect_equal(nrow(predict(m_reg, df_reg)), 506)
+  expect_equal(nrow(fitted(m_reg)), 506)
+  expect_equal(nrow(resid(m_reg)), 506)
+  expect_equal(nrow(predict(m_reg, df_reg %>% select(-medv))), 506)
+
+})
+
+test_that("enet classification works", {
+  library(dplyr)
+  df_cls3 <- iris
+  df_cls2 <- iris %>% filter(Species != "setosa")
+
+  m_reg <- classify(df_cls3, Species ~ ., m("enet", lambda = c(0.1), alpha = 0.1))
+  expect_equal(nrow(coef(m_reg)), 14)
+  expect_equal(nrow(predict(m_reg, df_cls3)), 450)
+  expect_equal(nrow(fitted(m_reg)), 450)
+  expect_equal(nrow(predict(m_reg, df_cls3 %>% select(-Species))), 450)
+
+})
+
+test_that("enet classification (2class) works", {
+  library(dplyr)
+  df_cls3 <- iris
+  df_cls2 <- iris %>% filter(Species != "virginica")
+
+  m_reg <- classify(df_cls2, Species ~ ., m("enet", lambda = c(0.01), alpha = 0.1))
+  expect_equal(nrow(coef(m_reg)), 5)
+  expect_equal(nrow(predict(m_reg, df_cls2)), 100)
+  expect_equal(nrow(fitted(m_reg)), 100)
+  expect_equal(nrow(predict(m_reg, df_cls2 %>% select(-Species))), 100)
+
+})
