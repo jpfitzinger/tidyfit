5a25deb5f9431fcd1ba0112515c27237a65153bd
Johann Pfitzinger <jpfitzinger@xmlh.de>
2023-05-20 12:52:01 +0200

    unit tests for spikeslab

---
+++ tests/testthat/test-spikeslab.R
@@ -0,0 +1,27 @@
+test_that("spikeslab regression works", {
+  library(dplyr)
+  df_reg <- MASS::Boston
+  df_cls3 <- iris
+  df_cls2 <- iris %>% filter(Species != "setosa")
+
+  m_reg <- m("spikeslab", medv ~ ., df_reg)
+  expect_equal(nrow(coef(m_reg)), 14)
+  expect_equal(nrow(predict(m_reg, df_reg)), 506)
+  expect_equal(nrow(fitted(m_reg)), 506)
+  expect_equal(nrow(resid(m_reg)), 506)
+  expect_equal(nrow(predict(m_reg, df_reg %>% select(-medv))), 506)
+
+})
+
+test_that("spikeslab classification works", {
+  library(dplyr)
+  df_cls3 <- iris
+  df_cls2 <- iris %>% filter(Species != "virginica")
+
+  m_reg <- classify(df_cls2, Species ~ ., m("spikeslab"))
+  expect_equal(nrow(coef(m_reg)), 5)
+  expect_equal(nrow(predict(m_reg, df_cls2)), 100)
+  expect_equal(nrow(fitted(m_reg)), 100)
+  expect_equal(nrow(predict(m_reg, df_cls2 %>% select(-Species))), 100)
+
+})
