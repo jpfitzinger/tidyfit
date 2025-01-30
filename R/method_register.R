f62b3183eebcdd05dafbadeeca1543b46309aa86
Johann Pfitzinger <jpfitzinger@xmlh.de>
2025-01-30 07:59:41 +0100

    add list_args option to METHOD class

--- R/method_register.R
+++ R/method_register.R
@@ -6,7 +6,8 @@ METHOD <- R6::R6Class(
     method = NULL,
     estimator = NULL,
     vector_args = NULL,
-    tunable_vector_args = NULL,
+    list_args = NULL,
+    no_grid_args = NULL,
     cv = NULL,
     regress = NULL,
     classify = NULL,
@@ -19,14 +20,15 @@ METHOD <- R6::R6Class(
 
     initialize = function(method, estimator, regress, classify, multinomial,
                           cv, uses_index, nonstandard_formula,
-                          vector_args, tunable_vector_args,
+                          vector_args, list_args, no_grid_args,
                           has_predict_method, has_coef_method, has_importance_method) {
       self$method <- method
       self$estimator <- estimator
       self$regress <- regress
       self$classify <- classify
       self$vector_args <- vector_args
-      self$tunable_vector_args <- tunable_vector_args
+      self$list_args <- list_args
+      self$no_grid_args <- no_grid_args
       self$multinomial <- multinomial
       self$cv <- cv
       self$uses_index <- uses_index
@@ -42,48 +44,57 @@ METHOD_REGISTER <- list(
   adalasso = METHOD$new(
     method = "adalasso", estimator = "glmnet::glmnet", regress = TRUE, classify = TRUE,
     multinomial = TRUE, cv = TRUE, uses_index = FALSE, nonstandard_formula = FALSE,
-    vector_args = c("weights", "offset", "lambda", "exclude", "lower.limits", "upper.limits"), tunable_vector_args = c("lambda", "lower.limits", "upper.limits"),
+    vector_args = c("weights", "offset", "lambda", "exclude", "lower.limits", "upper.limits"),
+    list_args = c(),
+    no_grid_args = c("weights"),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = TRUE
   ),
   anova = METHOD$new(
     method = "anova", estimator = "stats::anova", regress = TRUE, classify = TRUE,
     multinomial = FALSE, cv = FALSE, uses_index = FALSE, nonstandard_formula = FALSE,
-    vector_args = c("weights", "subset", "offset", "contrasts", "control"), tunable_vector_args = c("subset", "control"),
+    vector_args = c("weights", "subset", "offset"),
+    list_args = c("control", "contrasts"),
+    no_grid_args = c("weights"),
     has_predict_method = FALSE, has_coef_method = TRUE, has_importance_method = FALSE
   ),
   bayes = METHOD$new(
     method = "bayes", estimator = "arm::bayesglm", regress = TRUE, classify = TRUE,
     multinomial = FALSE, cv = FALSE, uses_index = FALSE, nonstandard_formula = FALSE,
-    vector_args = c("subset", "weights", "contrasts", "offset", "start", "etastart", "mustart", "control", "prior.mean", "prior.scale", "prior.df"),
-    tunable_vector_args = c("subset", "start", "etastart", "mustart", "control", "prior.mean", "prior.scale", "prior.df"),
+    vector_args = c("subset", "weights", "offset", "start", "etastart", "mustart", "prior.mean", "prior.scale", "prior.df"),
+    list_args = c("contrasts", "control"),
+    no_grid_args = c("weights"),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = TRUE
   ),
   blasso = METHOD$new(
     method = "blasso", estimator = "monomvn::blasso", regress = TRUE, classify = FALSE,
     multinomial = FALSE, cv = FALSE, uses_index = FALSE, nonstandard_formula = FALSE,
     vector_args = c("beta", "rd", "ab"),
-    tunable_vector_args = c("beta", "rd", "ab"),
+    list_args = c(),
+    no_grid_args = c(),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = FALSE
   ),
   bma = METHOD$new(
     method = "bma", estimator = "BMS::bms", regress = TRUE, classify = FALSE,
     multinomial = FALSE, cv = FALSE, uses_index = FALSE, nonstandard_formula = FALSE,
     vector_args = c("start.value"),
-    tunable_vector_args = c("start.value"),
+    list_args = c(),
+    no_grid_args = c(),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = FALSE
   ),
   bridge = METHOD$new(
     method = "bridge", estimator = "monomvn::bridge", regress = TRUE, classify = FALSE,
     multinomial = FALSE, cv = FALSE, uses_index = FALSE, nonstandard_formula = FALSE,
     vector_args = c("beta", "rd", "ab"),
-    tunable_vector_args = c("beta", "rd", "ab"),
+    list_args = c(),
+    no_grid_args = c(),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = FALSE
   ),
   boost = METHOD$new(
     method = "boost", estimator = "mboost::glmboost", regress = TRUE, classify = TRUE,
     multinomial = FALSE, cv = TRUE, uses_index = FALSE, nonstandard_formula = FALSE,
-    vector_args = c("weights", "contrasts.arg", "offset", "control", "oobweights"),
-    tunable_vector_args = c("control"),
+    vector_args = c("weights", "offset", "oobweights"),
+    list_args = c("control", "contrasts.arg"),
+    no_grid_args = c("control"),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = FALSE
   ),
   enet = METHOD$new(
@@ -95,7 +106,9 @@ METHOD_REGISTER <- list(
   lasso = METHOD$new(
     method = "lasso", estimator = "glmnet::glmnet", regress = TRUE, classify = TRUE,
     multinomial = TRUE, cv = TRUE, uses_index = FALSE, nonstandard_formula = FALSE,
-    vector_args = c("weights", "offset", "lambda", "exclude", "lower.limits", "upper.limits"), tunable_vector_args = c("lambda", "lower.limits", "upper.limits"),
+    vector_args = c("weights", "offset", "lambda", "exclude", "lower.limits", "upper.limits"),
+    list_args = c(),
+    no_grid_args = c("weights"),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = TRUE
   ),
   lm = METHOD$new(
@@ -107,7 +120,9 @@ METHOD_REGISTER <- list(
   ridge = METHOD$new(
     method = "ridge", estimator = "glmnet::glmnet", regress = TRUE, classify = TRUE,
     multinomial = TRUE, cv = TRUE, uses_index = FALSE, nonstandard_formula = FALSE,
-    vector_args = c("weights", "offset", "lambda", "exclude", "lower.limits", "upper.limits"), tunable_vector_args = c("lambda", "lower.limits", "upper.limits"),
+    vector_args = c("weights", "offset", "lambda", "exclude", "lower.limits", "upper.limits"),
+    list_args = c(),
+    no_grid_args = c("weights"),
     has_predict_method = TRUE, has_coef_method = TRUE, has_importance_method = TRUE
   )
 )
